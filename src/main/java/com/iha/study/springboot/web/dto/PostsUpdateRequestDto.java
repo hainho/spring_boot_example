8592896126eed6a8621e50390ebe823959adc9e1
hainho <hainho9704@gmail.com>
2022-03-20 23:06:46 +0900

    ✨feature : create post save, update, delete API and mustache

---
+++ src/main/java/com/iha/study/springboot/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,19 @@
+package com.iha.study.springboot.web.dto;
+
+import com.iha.study.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+    }
+}
